c4e8b9abf7f75e66ade1d003eaae1f61a4b90382
TheRealSlimStefan <66061781+TheRealSlimStefan@users.noreply.github.com>
2022-03-29 19:22:31 +0200

    Exercise - 29024

--- exercicio 4/exercicio/script.js
+++ exercicio 4/exercicio/script.js
@@ -1,2 +1,31 @@
-// flickr api key: https://www.flickr.com/services/api/misc.api_keys.html || a5dbe90ceeb8a3561dfc760ae33be5f7
-// Weather api key: https://home.openweathermap.org/users/sign_in || a9e49b305a5d14cc0790dbf507251ca1
+$(document).ready(function(){
+    const input = document.getElementById('search');
+    console.log(input.value);
+
+    const imagesContainer = document.querySelector('div.imagesContainer');
+
+    $("button").click(function(){
+        if(input.value !== ""){
+            $.ajax({url: `https://www.flickr.com/services/rest/`, data: {text: input.value, method: "flickr.photos.search", api_key: "3bb1cd6dadcf5cada04ddb01190a3fb5", extras: 'url_m', format: 'json', nojsoncallback: 1, api_sig: '14fb5b562f8c773e1a9d6f51589d64b6'} ,success: function(result){
+                    if(result.stat === "ok"){
+                        for(let i = 0; i < result.photos.photo.length; i++){
+                            const img = document.createElement("img");
+                            img.setAttribute("src", result.photos.photo[i].url_m)
+                            console.log(img);
+
+                            // console.log(result.photos.photo[i].url_m);
+                            imagesContainer.appendChild(img)
+                        }
+                    } else {
+                        alert(`couldn't find this phrase`);
+                    }
+
+
+
+                    //$("#div1").html(result);
+                }, error: function(){alert(`couldn't find this phrase`)}});
+        } else {
+            alert('The input cant be empty!');
+        }
+    });
+});
