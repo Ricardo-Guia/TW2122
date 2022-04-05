d30cc651d73b9fbc0ab1843f6c07d5e8cf420845
Joao Mirra 24555 <joaomirra@ipvc.pt>
2022-04-05 16:55:10 +0100

    s

---
+++ exercicio 4/exercicio 24555/script.js
@@ -0,0 +1,13 @@
+// flickr api key: https://www.flickr.com/services/api/misc.api_keys.html || a5dbe90ceeb8a3561dfc760ae33be5f7
+// Weather api key: https://home.openweathermap.org/users/sign_in || a9e49b305a5d14cc0790dbf507251ca1
+
+function searchInFlick(){
+    const valueSearch = $( #searchInputID ).val();
+
+    $.ajax({
+        url: "",
+        body: {
+            text: valueSearch
+        }
+    })
+}
