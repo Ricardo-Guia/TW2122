45dc50cabae53aa400a06252cd8bf7c24b62ef49
RicardoGuia <51034291+Ricardo-Guia@users.noreply.github.com>
2022-04-06 09:45:07 +0100

    Exercicio 4 part 2 resolved

--- exercicio 4/exercicio/script.js
+++ exercicio 4/exercicio/script.js
@@ -18,13 +18,33 @@ function requestFlickrApi() {
         method: 'GET',
         success: function (response) {
 
-                $('#photosContainer').html('');
-                response.photos.photo.forEach(item => {
-                        const imageHTMLElement = $('<img>').attr('src', item.url_m)
-                        $('#photosContainer').append(imageHTMLElement);
-                    }
-                )
+            $('#photosContainer').html('');
+            response.photos.photo.forEach(item => {
+                    const imageHTMLElement = $('<img>').attr('src', item.url_m)
+                    $('#photosContainer').append(imageHTMLElement);
+                }
+            )
 
+        }
+    });
+}
+
+function requestMeteoApi() {
+    const cityValue = $('#inputMeteoID').val();
+
+    $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/weather',
+        data: {
+            q: cityValue,
+            appid: 'a9e49b305a5d14cc0790dbf507251ca1',
+            units: 'metric',
+            lang: 'PT'
+        },
+        method: 'GET',
+        success: function (response) {
+            console.log(response);
+            const temperature = response.main.temp;
+            $('#meteoContainer').html('A temperatura em ' + response.name + ' é de: ' + temperature);
         }
     });
 }
